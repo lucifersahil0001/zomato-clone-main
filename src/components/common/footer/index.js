846ba0712b9d40ff86f29b8e1287dce99ffd13bb
lucifersahil0001 <harshgupta00010@gmail.com>
2023-07-17 22:27:19 +0530

    final-zomato

---
+++ src/components/common/footer/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../footer/footer.css";
+
+const Footer = () => {
+  return <div className="absolute-center">Made with ❤️ by Harsh Gupta</div>;
+};
+
+export default Footer;
